14009dbfbaa1048fdc88a86754757d051edbb321
CORTEX-0241\raul.dionisio_cortex <raul.dionisio@cortex-intelligence.com>
2024-01-03 18:16:17 -0300

    Adicionando roteamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 
-const routes: Routes = [];
+/*
+  Pra cada rota eu passo um objeto com o caminho
+ */
+const routes: Routes = [
+
+  {
+    path: "", 
+    redirectTo: "listarPensamento",
+    pathMatch: 'full' //Quando criar um path vazio precisamos passar o pathMatch
+  },
+  {
+    path: "criarPensamento", 
+    component: CriarPensamentoComponent 
+  },
+  
+  {
+    path: "listarPensamento", 
+    component: ListarPensamentoComponent 
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

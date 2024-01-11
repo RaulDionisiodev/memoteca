b44e7021ef6c7b5e4d2d8f0a009ded7f9d63d768
CORTEX-0241\raul.dionisio_cortex <raul.dionisio@cortex-intelligence.com>
2024-01-11 18:03:47 -0300

    Funcionalidade de edição de pensamentos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
 
 /*
   Pra cada rota eu passo um objeto com o caminho
@@ -27,6 +28,10 @@ const routes: Routes = [
   {
     path: "pensamentos/excluirPensamento/:id", 
     component: ExcluirPensamentoComponent 
+  },
+  {
+    path: "pensamentos/editarPensamento/:id", 
+    component: EditarPensamentoComponent 
   }
 
 ];

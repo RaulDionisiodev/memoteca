c4df1769a60364320dbf36f857f429114921ecbd
Raul Dionisio <rauldionisiosh@gmail.com>
2024-01-18 17:25:08 -0300

    Criação de formulario reativo

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,21 +10,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./criar-pensamento.component.css']
 })
 export class CriarPensamentoComponent implements OnInit {
+  formulario! : FormGroup;
 
-  pensamento : Pensamento = {
-    conteudo: "",
-    autoria: '',
-    modelo: 'modelo3'
-  }
   constructor(private servive : PensamentoService,
-    private router : Router  
+    private router : Router,
+    private formbuilder : FormBuilder  
   ) { }
 
   ngOnInit(): void {
+
+    this.formulario = this.formbuilder.group({
+      conteudo: ['formulário reativo'],
+      autoria: ['Angular'],
+      modelo: "modelo1"
+    })
   }
 
   criarPensamento() {
-    this.servive.criar(this.pensamento).subscribe(() => {
+    this.servive.criar(this.formulario.value).subscribe(() => {
       this.router.navigate(["/listarPensamento"])
     })
   }
